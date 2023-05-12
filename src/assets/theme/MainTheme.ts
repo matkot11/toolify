e5b85f172e67ca0576abaaee87bc082f5681694a
matkot11 <matkot11@gmail.com>
2023-05-12 22:01:46 +0200

    add global styles and theme

---
+++ src/assets/theme/MainTheme.ts
@@ -0,0 +1,28 @@
+export const theme = {
+  color: {
+    black: "#000000",
+    white: "#ffffff",
+    blue: "#006CBB",
+    green: "#36C982",
+    red: "#EB2424"
+  },
+  fontFamily: {
+    arapey: "Arapey, serif",
+    montserrat: "Montserrat, sans-serif"
+  },
+  fontWeight: {
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700
+  },
+  fontSize: {
+    font12: "0.75rem",
+    font14: "0.875rem",
+    font16: "1rem",
+    font18: "1.125rem",
+    font20: "1.25rem",
+    font24: "1.5rem",
+    font50: "3.125rem"
+  }
+};
