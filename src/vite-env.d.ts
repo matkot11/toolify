0a6482d33e2e46de5fc2ee9fcd130917f43219fe
Mateusz Kocik <44774471+matkot11@users.noreply.github.com>
2023-05-19 22:25:15 +0200

    Revert "add sanity"

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,8 +7,6 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
   readonly VITE_FIREBASE_APP_ID: string;
-  readonly VITE_SANITY_PROJECT_ID: string;
-  readonly VITE_SANITY_TOKEN: string;
 }
 
 interface ImportMeta {
