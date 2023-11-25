ee407e0c499f34a499762a78945499ef93982980
mateuszkocik <mateusz.kocik@infermedica.com>
2023-11-25 13:53:30 +0100

    Add prettier and css variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,12 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
   ],
   rules: {
-    'vue/multi-word-component-names': 'off',
+    "vue/multi-word-component-names": "off",
   },
-}
+};
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,22 +5,29 @@
  */
 
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import "@/styles/main.css";
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, ThemeDefinition } from "vuetify";
+
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#ffffff",
+    secondary: "#000000",
+    error: "#EB2424",
+    success: "#1e9559",
+  },
+};
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
+    defaultTheme: "lightTheme",
     themes: {
-      light: {
-        colors: {
-          primary: '#1867C0',
-          secondary: '#5CBBF6',
-        },
-      },
+      lightTheme,
     },
   },
-})
+});
