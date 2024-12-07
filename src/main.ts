8b9e1fae069f8db5e9bfbcada84b5169ff830f7b
mateuszkocik <mateusz.kocik@infermedica.com>
2024-12-07 11:58:45 +0100

    feature: Add icons

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,32 @@
 import {createApp} from 'vue';
 import {createPinia} from 'pinia';
-import 'vuetify/styles';
 import {createVuetify} from 'vuetify';
+import {aliases, mdi} from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
+
+import 'vuetify/styles';
+import '@mdi/font/css/materialdesignicons.css';
 
 import App from './App.vue';
 import router from './router';
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
   components,
   directives,
 });
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
-app.use(vuetify).mount('#app');
+app.mount('#app');
