0b6e1cdb611ef4b47687720710e99a32daf241dc
mateuszkocik <mateusz.kocik@infermedica.com>
2024-12-08 12:24:57 +0100

    feature: Add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
   import {RouterView} from 'vue-router';
+  import CommonsNavbar from '@/commons/components/CommonsNavbar.vue';
 </script>
 
 <template>
+  <CommonsNavbar />
   <RouterView />
 </template>
