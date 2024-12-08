3c5079fac4a42a7b9983c294e271593b55f8dcb0
mateuszkocik <mateusz.kocik@infermedica.com>
2024-12-08 12:23:02 +0100

    feature: Add css variables

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import {aliases, mdi} from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
+import '@/commons/styles/index.scss';
+
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
 
